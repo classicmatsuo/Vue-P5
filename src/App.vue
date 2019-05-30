81bb6a3745f11ee25890365ebd0ffdb3ccbce2b2
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2019-05-30 17:57:25 -0500

    web gl in osc.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Osc msg="Osc staging"></Osc>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Osc from './components/Osc.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Osc
   }
 }
 </script>
